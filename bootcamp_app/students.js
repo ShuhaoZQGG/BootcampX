5846a7f3793ecbbf2762d32cf4b2564b1f890cc7
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-13 22:24:00 -0400

    replace query by parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,17 +6,20 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-const CLI = process.argv.slice(2);
-const cohort = CLI[0];
-const limit = CLI[1];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name  '%${cohort}%'
-LIMIT ${limit};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);  })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,19 @@ const pool = new Pool({
 });
 const CLI = process.argv.slice(2);
 const cohort = CLI[0];
+const values = [`%${cohort}%`]
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id 
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);  })
